6addc94e1d55b6b16cef67438846396dc7c3a61b
Linkraft <36479127+Linkraft@users.noreply.github.com>
2019-09-03 16:53:35 -0400

    Removed weird README

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var http = require('http'),
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
-
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -31,6 +29,7 @@ var requestHandler = function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end(listingData);
   }
+  // Optionally ignore the /favicon.ico path in Chrome
   // else if (pathname == "/favicon.ico") {
   //   response.writeHead(200, {'Content-Type': 'text/plain'});
   //   response.end();
@@ -44,20 +43,20 @@ var requestHandler = function(request, response) {
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+  This callback function should save the data in the listingData variable, 
+  then start the server. 
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+  HINT: Check out this resource on fs.readFile
+  //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 
-    HINT: Read up on JSON parsing Node.js
-   */
+  HINT: Read up on JSON parsing Node.js
+  */
 
-    //Check for errors
-    if (err) console.error("Error occurred in reading listings.json: ", err);
+  //Check for errors
+  if (err) console.error("Error occurred in reading listings.json: ", err);
 
-    //Save the state in the listingData variable already defined
-    listingData = JSON.parse(data);
+  //Save the state in the listingData variable already defined
+  listingData = JSON.parse(data);
 
   //Creates the server
   server = http.createServer(requestHandler);
